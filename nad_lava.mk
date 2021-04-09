7936e7b13bb07617921d662e94cb46433d6f805c
toxiclord501 <wahyuamri501@gmail.com>
2021-04-09 23:48:51 +0000

    lava: add nad Official tag
    Signed-off-by: toxiclord501 <wahyuamri501@gmail.com>

--- nad_lava.mk
+++ nad_lava.mk
@@ -31,6 +31,9 @@ $(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 
+# Official tag
+NAD_BUILD_TYPE := OFFICIAL
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := lava
 PRODUCT_NAME := nad_lava
