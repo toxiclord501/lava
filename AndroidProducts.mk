f6a27199eee364e1afee10b4b6b4ca4331fc89ce
toxiclord501 <wahyuamri501@gmail.com>
2021-07-24 11:00:52 +0000

    lava: Nusantara-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-   $(LOCAL_DIR)/lineage_lava.mk
+   $(LOCAL_DIR)/nad_lava.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lava-user \
-    lineage_lava-userdebug \
-    lineage_lava-eng
+    nad_lava-user \
+    nad_lava-userdebug \
+    nad_lava-eng
